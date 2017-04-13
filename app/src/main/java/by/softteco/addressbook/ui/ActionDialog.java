73ddfd56b14588745ef72d76478aaaa5adf0d83d
kirill <bv_1514@mail.ru>
2017-04-13 20:56:39 +0300

    Showing data from db

---
+++ app/src/main/java/by/softteco/addressbook/ui/ActionDialog.java
@@ -0,0 +1,18 @@
+package by.softteco.addressbook.ui;
+
+import android.support.v4.app.DialogFragment;
+import by.softteco.addressbook.R;
+
+/**
+ * Created by kirila on 13.4.17.
+ */
+
+public class ActionDialog extends DialogFragment {
+
+    private final int LAYOUT = R.layout.dialog;
+
+    public static ActionDialog newInstance(){
+
+    }
+
+}
